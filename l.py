ce2c1d97fe337245d0d6c478a03b905928eb7221
jordiaphane <23087175+jordiaphane@users.noreply.github.com>
2020-07-09 13:23:39 -0700

    Update l.py

--- l.py
+++ l.py
@@ -1,6 +1,5 @@
-import sys, os, io
+import sys, os, io, re
 import lyricsgenius
-import re
 import webbrowser
 artistname = input('artist:')
 songtitle = input('song:')
@@ -9,22 +8,33 @@ geniusgenius = lyricsgenius.Genius("YOURAPIHERE")
 song = geniusgenius.search_song(
 
 
-songtitle,
-artistname) 
+
+songtitle
+, artistname) 
 songlyrics = song.lyrics
 print(song.lyrics)
 
 
-filename = "L/%s_%s.txt" % (artistname, songtitle)
+filename = "C:/Users/jornado/AS/DATA/L/%s_%s.txt" % (artistname, songtitle)
 with open(filename, 'w') as f:
 	f.write(songlyrics)
 	f.close()
 
-print("\n")
-yt = input('search on youtube?:')
-if yt == 'y':
-	songtitle1 = re.sub("[ ]","+", songtitle)
-	artistname1 = re.sub("[ ]","+", artistname)
-	url = "https://music.youtube.com/search?q=" + artistname1 + "+" + songtitle1
-	print(url)
+songsearch = input('search on youtube?')
+
+if songsearch == 'y':
+	plusartist = str(re.sub(' ', '+', artistname))
+	plussong = str(re.sub(' ', '+', songtitle))
+	url = 'https://music.youtube.com/search?q=' + plusartist + ' ' + plussong
 	webbrowser.open(url)
+"""### to use this
+you need to download and install ffmpeg from: https://ffmpeg.org/
+lets say you install it to c:/ProgramData/ffmpeg, you then need to add that bin to your system PATH"""
+download = input('download? y/n')
+if download == 'y':
+	oorl = input('copy url here')
+
+	os.chdir('L')
+	os.mkdir('dls')
+
+	os.system('youtube-dl -x --audio-format mp3 ' + oorl)
